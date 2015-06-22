cb61dae5ac0a583dbf523bcfdc81b6002232ec35
Sumit Maharjan <timusumit@gmail.com>
2015-06-22 17:15:11 +0545

    sace
    sa

---
+++ login.php
@@ -0,0 +1,31 @@
+<?php include('includes/head.php'); ?>
+
+<!-- header part start-->
+<?php include('includes/header.php'); ?>
+<!-- header part end-->
+<style>
+.head_main{
+opacity:1 !important;
+}
+</style>
+<!-- content area-->
+<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 cont-main">
+<div class="col-lg-4 col-md-4 col-sm-3 hidden-xs">
+</div>
+<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12" style="background:#f9f9f9; padding:20px;">
+<form>
+<input type="text" class="form-control" placeholder="Username">
+<br>
+<input type="password" class="form-control" placeholder="Password">
+<button class="btn btn-success"  type="submit">Submit</button>
+</form>
+</div>
+<div class="col-lg-4 col-md-4 col-sm-3 hidden-xs">
+</div>
+</div>
+<!-- content area-->
+
+<!-- footer part-->
+<?php include('includes/footer.php'); ?>
+<!-- footer part-->
+
